31ef2c56f584a761cf329311232c3ccf8c87a6cc
cesa <alessio.cesaroni@live.com>
2026-07-25 16:56:56 +0200

    tolgo i csrf dalle chiamate api react

--- siw-polls-react/src/service/api.ts
+++ siw-polls-react/src/service/api.ts
@@ -1,19 +1,14 @@
 import axios from "axios";
 import { BACKEND_URL } from '../components/config'; 
+
 const apiRest = axios.create({
   baseURL: BACKEND_URL + '/rest/',
-  withCredentials: true,
-  withXSRFToken: true,  
-  xsrfCookieName: 'XSRF-TOKEN', 
-  xsrfHeaderName: 'X-XSRF-TOKEN'
+  withCredentials: true
 });
 
 export const api = axios.create({
   baseURL: BACKEND_URL,
-  withCredentials: true,
-  withXSRFToken: true,  
-  xsrfCookieName: 'XSRF-TOKEN', 
-  xsrfHeaderName: 'X-XSRF-TOKEN'
+  withCredentials: true
 });
 
 export default apiRest;
